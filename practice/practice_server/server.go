90828749078c1737ee6a3484fc7bf0c96e227aea
yujialves <yujialves@i.softbank.jp>
2020-09-15 15:12:20 +0900

    Implement Reflection

--- practice/practice_server/server.go
+++ practice/practice_server/server.go
@@ -11,6 +11,7 @@ import (
 
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/reflection"
 	"google.golang.org/grpc/status"
 
 	"../practicepb"
@@ -126,6 +127,8 @@ func main() {
 
 	s := grpc.NewServer(opts...)
 	practicepb.RegisterPracticeServiceServer(s, &server{})
+
+	reflection.Register(s)
 
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("Failed to serve: %v", err)
